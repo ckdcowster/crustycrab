f355c60654e0ea732e9b099d75b98c05bab30244
Neil Cowie <thecowster@hotmail.co.uk>
2025-03-22 16:37:58 +0000

    Hands-on exercise #50 - add a record

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 /*
 
-Hands-on exercise #49 - map[string][]string
-Create a map with a key of TYPE string which is a person’s “last_first” name, and a value of
-TYPE []string which stores their favorite things. Store three records in your map. Print out all of
-the values, along with their index position in the slice.
-
-key 				value
-`bond_james` 		`shaken, not stirred`, `martinis`, `fast cars`
-`moneypenny_jenny` 	`intelligence`, `literature`, `computer science`
-`no_dr` 			`cats`, `ice cream`, `sunsets`
+Hands-on exercise #50 - add a record
+Using the code from the previous example, add a record to your map. Now print the map out
+using the “range” loop
+key 			value
+`fleming_ian` 	`steaks`, `cigars`, `espionage`
 
 */
 
@@ -23,6 +19,15 @@ func main() {
 	my_map[`moneypenny_jenny`] = []string{`intelligence`, `literature`, `computer science`}
 	my_map[`no_dr`] = []string{`cats`, `ice cream`, `sunsets`}
 
+	for k, v := range my_map {
+		fmt.Println("key is", k)
+		for i, s := range v {
+			fmt.Printf("idx = %v \tvalue = %v\n", i, s)
+		}
+	}
+
+	fmt.Println("-------------------------------")
+	my_map[`fleming_ian`] = []string{`steaks`, `cigars`, `espionage`}
 	for k, v := range my_map {
 		fmt.Println("key is", k)
 		for i, s := range v {
